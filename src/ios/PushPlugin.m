f9593712bca2274d9c1a2e233f4f7a971f7929e8
Jonathan Nunez <jonathan.naguin@gmail.com>
2013-10-04 12:03:16 +0100

    iOS sending extras parameter to the callback

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -1,16 +1,16 @@
 /*
  Copyright 2009-2011 Urban Airship Inc. All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
-
+ 
  1. Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
-
+ 
  2. Redistributions in binaryform must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided withthe distribution.
-
+ 
  THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
@@ -37,18 +37,18 @@
 
 - (void)unregister:(CDVInvokedUrlCommand*)command;
 {
-    self.callbackId = command.callbackId;
-
+	self.callbackId = command.callbackId;
+    
     [[UIApplication sharedApplication] unregisterForRemoteNotifications];
     [self successWithMessage:@"unregistered"];
 }
 
 - (void)register:(CDVInvokedUrlCommand*)command;
 {
-    self.callbackId = command.callbackId;
-
+	self.callbackId = command.callbackId;
+    
     NSMutableDictionary* options = [command.arguments objectAtIndex:0];
-
+    
     UIRemoteNotificationType notificationTypes = UIRemoteNotificationTypeNone;
     id badgeArg = [options objectForKey:@"badge"];
     id soundArg = [options objectForKey:@"sound"];
@@ -57,71 +57,77 @@
     if ([badgeArg isKindOfClass:[NSString class]])
     {
         if ([badgeArg isEqualToString:@"true"])
-            notificationTypes |= UIRemoteNotificationTypeBadge;
+        notificationTypes |= UIRemoteNotificationTypeBadge;
     }
     else if ([badgeArg boolValue])
-        notificationTypes |= UIRemoteNotificationTypeBadge;
+    notificationTypes |= UIRemoteNotificationTypeBadge;
     
     if ([soundArg isKindOfClass:[NSString class]])
     {
         if ([soundArg isEqualToString:@"true"])
-            notificationTypes |= UIRemoteNotificationTypeSound;
+        notificationTypes |= UIRemoteNotificationTypeSound;
     }
     else if ([soundArg boolValue])
-        notificationTypes |= UIRemoteNotificationTypeSound;
+    notificationTypes |= UIRemoteNotificationTypeSound;
     
     if ([alertArg isKindOfClass:[NSString class]])
     {
         if ([alertArg isEqualToString:@"true"])
-            notificationTypes |= UIRemoteNotificationTypeAlert;
+        notificationTypes |= UIRemoteNotificationTypeAlert;
     }
     else if ([alertArg boolValue])
-        notificationTypes |= UIRemoteNotificationTypeAlert;
+    notificationTypes |= UIRemoteNotificationTypeAlert;
     
     self.callback = [options objectForKey:@"ecb"];
-
-    if (notificationTypes == UIRemoteNotificationTypeNone)
-        NSLog(@"PushPlugin.register: Push notification type is set to none");
-
-    isInline = NO;
-
-    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:notificationTypes];
     
-    if (notificationMessage)            // if there is a pending startup notification
-        [self notificationReceived];    // go ahead and process it
+    if (notificationTypes == UIRemoteNotificationTypeNone)
+    NSLog(@"PushPlugin.register: Push notification type is set to none");
+    
+    isInline = NO;
+    
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:notificationTypes];
+	
+	if (notificationMessage)			// if there is a pending startup notification
+    [self notificationReceived];	// go ahead and process it
 }
 
 - (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-
+    
     NSString *token = [[[[deviceToken description] stringByReplacingOccurrencesOfString:@"<"withString:@""]
                         stringByReplacingOccurrencesOfString:@">" withString:@""]
                        stringByReplacingOccurrencesOfString: @" " withString: @""];
     
-
-    [self successWithMessage:[NSString stringWithFormat:@"%@", token]];
+    
+	[self successWithMessage:[NSString stringWithFormat:@"%@", token]];
 }
 
 - (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
-    [self failWithMessage:@"" withError:error];
+	[self failWithMessage:@"" withError:error];
 }
 
 - (void)notificationReceived {
     DLog(@"Notification received");
-
+    
     if (self.notificationMessage && self.callback)
     {
+        NSLog(@"notificationMessage:: %@", self.notificationMessage);
+        
         NSError *error;
         NSData *jsonData = [NSJSONSerialization dataWithJSONObject:[notificationMessage objectForKey:@"aps"]
                                                            options:0
                                                              error:&error];
+        NSData *jsonExtraData = [NSJSONSerialization dataWithJSONObject:[notificationMessage objectForKey:@"extra"]
+                                                                options:0
+                                                                  error:&error];
         
         if (!jsonData) {
             NSLog(@"Error parsing the notification message: %@", error);
         } else {
             NSString *jsonStr = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-            DLog(@"Msg: %@", jsonStr);
-            NSString * jsCallBack = [NSString stringWithFormat:@"%@(%@);", self.callback, jsonStr];
+            NSString *jsonExtraStr = [[NSString alloc] initWithData:jsonExtraData encoding:NSUTF8StringEncoding];
+            
+            NSString * jsCallBack = [NSString stringWithFormat:@"%@(%@, %@);", self.callback, jsonStr, jsonExtraStr];
             [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
         }
         
